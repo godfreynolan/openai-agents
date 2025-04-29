fda5ded74529f0f6ce6571f4860ced4caecc0178
Godfrey Nolan <godfrey@riis.com>
2025-04-29 12:06:20 -0400

    Update math_tutor.py

--- math_tutor.py
+++ math_tutor.py
@@ -46,8 +46,8 @@ async def main():
     result = await Runner.run(triage_agent, "who was the first president of the united states?")
     print(result.final_output)
 
-    result = await Runner.run(triage_agent, "what is life")
-    print(result.final_output)
+    #result = await Runner.run(triage_agent, "what is life")
+    #print(result.final_output)
 
 if __name__ == "__main__":
     asyncio.run(main())
